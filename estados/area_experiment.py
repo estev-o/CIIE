a062483853a604d67c86a61f0d89831d5e1e941d
estev-o <estevo.aldea@udc.es>
2026-02-03 21:57:26 +0100

    area de experimentos implementada

---
+++ estados/area_experiment.py
@@ -0,0 +1,22 @@
+import pygame, os
+from estados.estado import Estado
+from estados.hub import Player
+
+class AreaExperiment(Estado):
+    def __init__(self, juego):
+        Estado.__init__(self,juego)
+        # CARGAR FONDO AQUÍ
+
+        self.player = Player(self.juego)       
+
+    def actualizar(self, dt, acciones):
+        self.player.update(dt, acciones)
+
+    def dibujar(self, pantalla):
+        # Dibujar el Área de Experimento en la pantalla
+        pantalla.fill((100, 50, 50))  # Fondo rojo oscuro como ejemplo
+        # Puerta de salida (simple rectángulo) a la derecha en el centro
+        pygame.draw.rect(pantalla, (50, 200, 50), (self.juego.ancho - 100, self.juego.alto // 2 - 50, 80, 100))
+        
+        self.juego.draw_text(pantalla, "ÁREA DE EXPERIMENTOS", (255, 255, 255), self.juego.ancho // 2, self.juego.alto // 2)
+        self.player.render(pantalla)
--- estados/hub.py
+++ estados/hub.py
@@ -6,14 +6,30 @@ class Hub(Estado):
         Estado.__init__(self,juego)
         # CARGAR FONDO AQUÍ
 
+        # Puerta de salida (rectángulo) a la derecha en el centro
+        self.puerta_rect = pygame.Rect(
+            self.juego.ancho - 100,
+            self.juego.alto // 2 - 50,
+            80,
+            100,
+        )
+
         self.player = Player(self.juego)       
 
     def actualizar(self, dt, acciones):
         self.player.update(dt, acciones)
 
+        # Si el rect del jugador toca cualquier parte del rectángulo de la puerta, cambiar de área
+        if self.player.get_rect().colliderect(self.puerta_rect):
+            from estados.area_experiment import AreaExperiment
+            AreaExperiment(self.juego).entrar_estado()
+            return
+
     def dibujar(self, pantalla):
         # Dibujar el Hub en la pantalla
         pantalla.fill((50, 50, 100))  # Fondo azul oscuro como ejemplo
+        pygame.draw.rect(pantalla, (200, 100, 50), self.puerta_rect)
+
         self.juego.draw_text(pantalla, "HUB DEL JUEGO", (255, 255, 255), self.juego.ancho // 2, self.juego.alto // 2)
         self.player.render(pantalla)
 
@@ -56,6 +72,14 @@ class Player():
 
     def render(self, pantalla):
         pantalla.blit(self.curr_image, (int(self.pos_x), int(self.pos_y)))
+
+    def get_rect(self) -> pygame.Rect:
+        return pygame.Rect(
+            int(self.pos_x),
+            int(self.pos_y),
+            self.frame_w * self.scale,
+            self.frame_h * self.scale,
+        )
 
     def animate(self, dt, moving: bool):
         # Elegir animación según hacia dónde miramos
