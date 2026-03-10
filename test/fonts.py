43122243d6ad6b2f8d33917f755ebfe3419cd7c5
Pablo García López <pablo.glopez@udc.es>
2026-03-10 19:41:48 +0100

    delete test

--- test/fonts.py
+++
@@ -1,7 +0,0 @@
-import pygame
-class Fuentes:
-    def __init__(self):
-        self.small = pygame.font.Font("assets/fonts/Snowbell-Wp4g9.ttf", 16)
-        self.medium = pygame.font.Font("assets/fonts/Snowbell-Wp4g9.ttf", 24)
-        self.big = pygame.font.Font("assets/fonts/Snowbell-Wp4g9.ttf", 64)
-        self.level_title = pygame.font.Font("assets/fonts/Snowbell-Wp4g9.ttf", 42)
